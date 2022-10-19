1836421bc3b041a280af4bb631ecba484963d6d2
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-10-19 13:04:40 +0200

    Live coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from "./Hello";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const names = [
+        'Anton',
+        'Berta',
+        'Cäsar',
+    ]
+
+    return <>
+        {
+            names.map(name =>
+                <Hello name={name} key={name}></Hello>
+            )
+        }
+
+    </>
 }
-
-export default App;
---
+++ src/Hello.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Hello(props: {
+    name: string,
+}) {
+    return <h1>Hello {props.name}</h1>;
+}
